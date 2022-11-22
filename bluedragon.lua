ebdd2a08c37a5bafb4a28cb28635875114e1cb7b
AviansEpic <64453220+AviansEpic@users.noreply.github.com>
2022-11-22 14:11:03 -0700

    Update bluedragon.lua

--- bluedragon.lua
+++ bluedragon.lua
@@ -1,3 +1,5 @@
 if game.PlaceId == 10053187005 then
-loadstring(game:HttpGet(""))()
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/AviansEpic/BlueDragon/main/CounterBlox.lua"))()
+else
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/AviansEpic/BlueDragon/main/Universal.lua"))
 end
